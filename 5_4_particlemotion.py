57700ed567a8af4c51faad284247fa10961c8356
SDSW-DrHicks <119824568+SDSW-DrHicks@users.noreply.github.com>
2023-02-19 17:03:22 -0500

    First commit

---
+++ 5_4_particlemotion.py
@@ -0,0 +1,74 @@
+# Simple pygame program
+
+# import this package
+import pygame  # in terminal: pip3 install pygame --pre
+import random
+# NEW
+import math
+
+# --------------------------------------------------------------------
+# Format a game window:
+background_color = (255, 255, 255)  # white
+(width, height) = (300, 200)  # size (pixels)
+screen = pygame.display.set_mode((width, height))
+
+# Name the window with a caption
+pygame.display.set_caption('Particles')
+
+
+# Drawing
+
+class Particle:  # a class, creates a new type of object where common features are stored.
+    def __init__(self, position, size):  # __init__ is a built-in function used for creating a class.
+        self.x, self.y = position  # "self" is used to specify objects of a class.
+        self.size = size
+        self.colour = (0, 0, 255)  # blue
+        self.thickness = 1
+        self.speed = 2  # new
+        self.angle = math.pi / 2  # new
+
+    def display(self):
+        pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size, self.thickness)
+
+    def motion(self):
+        self.x += math.sin(self.angle) * self.speed
+        self.y - + math.cos(self.angle) * self.speed  # y is negative since the y-axis on comp graphics is inverted
+
+
+particle_number = 20
+my_particles = []
+
+for n in range(particle_number):
+    size = random.randint(10, 20)
+    x = random.randint(size, width - size)  # subract to make sure all of particle is on screen
+    y = random.randint(size, height - size)
+    particle = Particle((x, y), size)
+    particle.speed = random.random()
+    particle.angle = random.uniform(0, math.pi / 2)
+    my_particles.append(Particle((x, y), size))  # append adds an element to a defined list
+
+# no longer needed:
+# my_first_particle = Particle((150, 50), 15) # arguments: Particle((x,y), radius)
+# my_first_particle.display() # display the image of the particle
+
+#
+
+# use the flip command to display the window/screen.
+pygame.display.flip()
+
+# To keep the window closed till we close it.
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
+    screen.fill(background_color)
+
+    for particle in my_particles:  # moved from earlier in the code so that simulation
+        # will run for as long as the code is running/screen is on.
+        particle.display()
+        particle.motion()  # NEW, to account for motion f(x)
+        pygame.display.flip()
+
